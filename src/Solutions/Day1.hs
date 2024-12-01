bf07d78be2beec91ca4d30d79ad37bf586ab1918
Raphael <rafi.colman@googlemail.com>
2024-12-01 08:03:16 +0000

    Day 1 parts 1 and 2

--- src/Solutions/Day1.hs
+++ src/Solutions/Day1.hs
@@ -1,30 +1,41 @@
 module Solutions.Day1
-  ( aoc1
-  ) where
+    ( aoc1
+    ) where
 
 import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
                                       printSolutions, printTestSolutions)
+import           Common.ListUtils    (freqs, window2, window3)
 import           Data.List           (tails)
-import           Text.Trifecta       (Parser, TokenParsing (token), integer,
+import qualified Data.Map.Strict     as M
+import           GHC.OldList         (sort)
+import           Text.Parser.Char    (digit)
+import           Text.Trifecta       (CharParsing (anyChar), Parser,
+                                      TokenParsing (token), count, integer,
                                       some)
-import Common.ListUtils (window3, window2)
 
 aoc1 :: IO ()
 aoc1 = do
   printSolutions 1 $ MkAoCSolution parseInput part1
   printSolutions 1 $ MkAoCSolution parseInput part2
 
-type Depths = [Integer]
-
-parseInput :: Parser Depths
+parseInput :: Parser [(Integer, Integer)]
 parseInput = do
-  some $ token integer
+  some $ token parsePair
+  where parsePair :: Parser (Integer, Integer)
+        parsePair = do
+            [a,b] <- count 2 integer
+            pure (a,b)
 
-part1 :: Depths -> Int
-part1 = sonarSweep
+part1 :: [(Integer, Integer)] -> Integer
+part1 input = sum $ abs <$> zipWith (-) l2 l1
+  where (l1, l2) = convetToSeparateLists input
 
-part2 :: Depths -> Int
-part2 = sonarSweep . map (\(x, y, z) -> x + y + z) . window3
 
-sonarSweep :: Depths -> Int
-sonarSweep = length . filter id . map (\(x, y) -> y > x) . window2
+part2 input = sum $ map (\x -> x * M.findWithDefault 0 x freqMap) l1
+  where (l1, l2) = convetToSeparateLists input
+        freqMap = freqs l2
+
+convetToSeparateLists :: [(Integer, Integer)] -> ([Integer], [Integer])
+convetToSeparateLists input = (l1, l2)
+  where l1 = sort $ map fst input
+        l2 = sort $ map snd input
