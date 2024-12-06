db01e7cca51fb14b2cd1bff8952f6e4cfed6683b
Raphael <rafi.colman@googlemail.com>
2024-12-06 08:45:05 +0000

    Day 6 part 1 complete

--- src/Solutions/Day6.hs
+++ src/Solutions/Day6.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module Solutions.Day6
@@ -11,16 +10,18 @@ import Common.AoCSolutions
     printSolutions,
     printTestSolutions,
   )
+import Common.EnumUtils (enumNext)
 import Common.Geometry (Grid, Point, enumerateMultilineStringToVectorMap, renderVectorMap)
 import Control.Lens ((^.))
 import Data.Foldable (maximumBy)
 import Data.Function (on)
+import Data.List
 import qualified Data.Map as M
 import qualified Data.Set as S
+import Debug.Trace
+import Linear (unit)
 import Linear.V2 (R1 (_x), R2 (_y), V2 (..))
 import Text.Trifecta (Parser, anyChar, some)
-import Linear (unit)
-import Common.EnumUtils (enumNext)
 
 data State = MkState
   { _guard :: !(V2 Int),
@@ -35,7 +36,7 @@ data Direction = North | East | South | West
 
 aoc6 :: IO ()
 aoc6 = do
-  printTestSolutions 6 $ MkAoCSolution parseInput part1
+  printSolutions 6 $ MkAoCSolution parseInput part1
 
 -- printSolutions 6 $ MkAoCSolution parseInput part2
 
@@ -43,8 +44,9 @@ parseInput :: Parser (Grid Char)
 parseInput = do
   enumerateMultilineStringToVectorMap <$> some anyChar
 
-part1 input = state
-  where state = initState input
+part1 input = numberOfDistinctGuardPositions $ runMachine state
+  where
+    state = initState input
 
 -- part2 :: String -> String
 -- part2 = undefined
@@ -57,28 +59,35 @@ initState grid = MkState guard North obstacles (V2 xMax yMax)
     (V2 xMax _) = maximumBy (compare `on` (^. _x)) $ M.keys grid
     (V2 _ yMax) = maximumBy (compare `on` (^. _y)) $ M.keys grid
 
-
-stepState :: State -> Maybe State
+stepState :: State -> State
 stepState state
-  | not (isInBounds state) = Nothing
-  | isTouchingObstacle state = Just $ turnRight state
-  | otherwise = walk
+  | isTouchingObstacle state = turnRight state
+  | otherwise = walk state
 
 isTouchingObstacle :: State -> Bool
-isTouchingObstacle MkState{..} = nextPoint `S.member` _obstacles
-  where nextPoint = _guard + directionToUnitVector _direction
+isTouchingObstacle MkState {..} = nextPoint `S.member` _obstacles
+  where
+    nextPoint = _guard + directionToUnitVector _direction
 
 turnRight :: State -> State
-turnRight s@MkState{..} = s { _direction = enumNext _direction }
+turnRight s@MkState {..} = s {_direction = enumNext _direction}
 
 isInBounds :: State -> Bool
 isInBounds (MkState (V2 gx gy) _ _ (V2 xMax yMax)) = gx >= 0 && gx <= xMax && gy >= 0 && gy <= yMax
 
 walk :: State -> State
-walk s@MkState{..} = s { _guard = _guard + directionToUnitVector _direction }
+walk s@MkState {..} = s {_guard = _guard + directionToUnitVector _direction}
+
+runMachine :: State -> [State]
+runMachine = takeWhile isInBounds . iterate stepState
+
+numberOfDistinctGuardPositions :: [State] -> Int
+numberOfDistinctGuardPositions state = length $ nub $ map _guard state
+  where
+    allPositions = nub $ map _guard state
 
 directionToUnitVector :: Direction -> V2 Int
-directionToUnitVector North     = -(unit _y)
-directionToUnitVector South     = unit _y
-directionToUnitVector East      = unit _x
-directionToUnitVector West      = -(unit _x)
+directionToUnitVector North = -(unit _y)
+directionToUnitVector South = unit _y
+directionToUnitVector East = unit _x
+directionToUnitVector West = -(unit _x)
