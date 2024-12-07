034f4ead4ef5614ed062cc99011fba0308216bdf
Raphael <rafi.colman@googlemail.com>
2024-12-07 14:32:04 +0000

    Day 7 part 2

--- src/Solutions/Day7.hs
+++ src/Solutions/Day7.hs
@@ -3,6 +3,7 @@ module Solutions.Day7
   )
 where
 
+import Combinatorics (permuteRep, variateRep)
 import Common.AoCSolutions
   ( AoCSolution (MkAoCSolution),
     printSolutions,
@@ -11,11 +12,12 @@ import Common.AoCSolutions
 import Text.Parser.Char (CharParsing (char, string))
 import Text.Parser.Token (integer, integer')
 import Text.Trifecta (Parser, Parsing (eof), TokenParsing (token), manyTill, newline, sepBy, some, try)
-import Combinatorics (permuteRep, variateRep)
+import Data.Maybe (catMaybes, mapMaybe)
 
 aoc7 :: IO ()
 aoc7 = do
-  printSolutions 7 $ MkAoCSolution parseInput part1
+  printTestSolutions 7 $ MkAoCSolution parseInput part1
+  printTestSolutions 7 $ MkAoCSolution parseInput part2
 
 type Equation = (Integer, [Integer])
 
@@ -28,39 +30,49 @@ parseEquation = do
   inputs <- sepBy integer' $ char ' '
   pure (x, inputs)
 
+-- At the moment this just does part 2.
 part1 input = sum $ map fst validEquations
-  where validEquations = filter equationIsValid input
+  where
+    validEquations = filter equationIsValid input
 
-part2 :: String -> String
-part2 = undefined
+part2 input = sum $ map fst validEquations
+  where
+    validEquations = filter equationIsValid input
 
 data Expression
   = Const Integer
   | Add
   | Mul
+  | Concat
 
 instance Show Expression where
   show (Const x) = show x
   show Add = "+"
   show Mul = "*"
+  show Concat = "||"
 
-
+-- >>> allOrders 2
+-- [[+,+],[+,*],[*,+],[*,*]]
 allOrders :: Int -> [[Expression]]
-allOrders length = variateRep length [Add, Mul]
+allOrders length = variateRep length [Add, Concat, Mul]
 
 generateExpressionLists :: [Integer] -> [[Expression]]
 generateExpressionLists xs = map equation $ allOrders (length xs - 1)
-  where equation = alternate (map Const xs)
+  where
+    equation = alternate (map Const xs)
 
-evaluate :: [Expression] -> Integer
-evaluate [Const i] = i
-evaluate (Const x : Add : Const y : rest) = evaluate $ Const (x + y) : rest
-evaluate (Const x : Mul : Const y : rest) = evaluate $ Const (x * y) : rest
+evaluate :: Integer -> [Expression] -> Maybe Integer
+evaluate target [Const i] = Just i
+evaluate target (Const x : Add : Const y : rest) = if x > target then Nothing else evaluate target $ Const (x + y) : rest
+evaluate target (Const x : Mul : Const y : rest) = if x > target then Nothing else evaluate target $ Const (x * y) : rest
+evaluate target (Const x : Concat : Const y : rest) = if x > target then Nothing else evaluate target $ Const (concatenateIntegers x y) : rest
+--There has GOT to be a neater way of doing this!
 
 -- | Can the test values be evaluated to the target value?
 equationIsValid :: Equation -> Bool
 equationIsValid (target, ints) = target `elem` results
-  where results = evaluate <$> generateExpressionLists ints
+  where
+    results = mapMaybe (evaluate target) $ generateExpressionLists ints
 
 -- >>> alternate [1..3] [7..10]
 -- [1,7,2,8,3,9,10]
@@ -69,3 +81,6 @@ alternate [] [] = []
 alternate xs [] = xs
 alternate [] ys = ys
 alternate (x : xs) (y : ys) = x : y : alternate xs ys
+
+concatenateIntegers :: Integer -> Integer -> Integer
+concatenateIntegers x y = read $ show x ++ show y
