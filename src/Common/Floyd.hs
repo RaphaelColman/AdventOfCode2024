bc1848a1ca86cae79f8963d1d8b11d1fd531d5e8
Raphael <rafi.colman@googlemail.com>
2024-12-08 17:10:59 +0000

    Day 6 part 2

--- src/Common/Floyd.hs
+++ src/Common/Floyd.hs
@@ -1,12 +1,17 @@
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Common.Floyd where
 
 import Common.ListUtils (dropUntil)
-import Control.Lens (makeLenses)
+import Control.Lens (makeLenses, (^.))
+import Control.Monad ((>=>))
+import Control.Monad.Extra (iterateM)
 import Data.Foldable (find)
 import Data.List (elemIndex)
-import Debug.Trace (traceM)
+import Debug.Trace (traceM, traceShow, traceShowM)
 import GHC.OldList (findIndex)
 
 data CycleData a
@@ -17,7 +22,14 @@ data CycleData a
   }
   deriving (Eq, Show)
 
+data HTState a = MkHTState
+  { _tortoise :: !a,
+    _hare :: !a
+  }
+  deriving (Eq, Show)
+
 makeLenses ''CycleData
+makeLenses ''HTState
 
 hareAndTortoise ::
   (Eq b) =>
@@ -36,29 +48,21 @@ hareAndTortoise f start eqFun = do
   where
     areEqual x y = eqFun x == eqFun y
 
--- | Hare are tortoise algorithm to find cycles.
--- If at any point the termination condition is satisfied, this just returns a Nothing
-hareAndTortoiseWithTermination ::
+hareAndTortoiseM ::
+  forall a b.
   (Eq b) =>
-  (a -> a) -> -- Function move from one node to the next
+  (a -> Maybe a) -> -- Function move from one node to the next. If it returns a Nothing then we stop the search
   a -> -- Initial state
   (a -> b) -> -- Function to compare if two nodes are equal
-  (a -> Bool) -> -- termination condition
-  Maybe (CycleData a)
-hareAndTortoiseWithTermination f start eqFun shouldStop = do
-  let hare = drop 1 $ iterate (f . f) start
-  let tortoise = drop 1 $ iterate f start
-  let pairs = zip hare tortoise
-  (t, h) <- findM (uncurry areEqualOrStop) pairs
-  (startIndex, (t', _)) <- find (\(_, y) -> uncurry areEqual y) $ zip [0 ..] $ zip (iterate f start) (iterate f h)
-  length <- findIndex (areEqual t') $ drop 1 $ iterate f t' -- This will be too low by 1 because I had to drop the first element
-  pure $ MkCycleData startIndex (length + 1) t'
+  Bool -- Just if there was a cycle. Can't be bothered to do the whole algorithm right now
+hareAndTortoiseM f start eqFun = go (MkHTState start start)
   where
-    areEqualOrStop x y
-      | shouldStop y = Nothing
-      | shouldStop x = Nothing
-      | otherwise = Just $ eqFun x == eqFun y
+    go htState = case progressState htState of
+      Nothing -> False --There was no cycle before we hit the end
+      Just newState -> ((newState ^. tortoise) `areEqual` (newState ^. hare)) || go newState
+    progressState :: HTState a -> Maybe (HTState a)
+    progressState htState = do
+      tortoise <- f (htState ^. tortoise)
+      hare <- (f >=> f ) (htState ^. hare)
+      pure $ MkHTState tortoise hare
     areEqual x y = eqFun x == eqFun y
-
-findM :: (Foldable t) => (a -> Maybe Bool) -> t a -> Maybe a
-findM f = find (\x -> f x == Just True)
--- src/Solutions/Day6.hs
+++ src/Solutions/Day6.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedRecordDot #-}
 
 module Solutions.Day6
   ( aoc6,
@@ -11,7 +12,7 @@ import Common.AoCSolutions
     printTestSolutions,
   )
 import Common.EnumUtils (enumNext)
-import Common.Geometry (Grid, Point, enumerateMultilineStringToVectorMap, renderVectorMap)
+import Common.Geometry (Grid, Point, enumerateMultilineStringToVectorMap, renderVectorMap, renderVectorSet)
 import Control.Lens ((^.))
 import Data.Foldable (maximumBy)
 import Data.Function (on)
@@ -22,7 +23,7 @@ import Debug.Trace
 import Linear (unit)
 import Linear.V2 (R1 (_x), R2 (_y), V2 (..))
 import Text.Trifecta (Parser, anyChar, some)
-import Common.Floyd (hareAndTortoiseWithTermination, CycleData)
+import Common.Floyd (CycleData, hareAndTortoiseM)
 import Common.Debugging
 
 data State = MkState
@@ -39,7 +40,7 @@ data Direction = North | East | South | West
 aoc6 :: IO ()
 aoc6 = do
   --printSolutions 6 $ MkAoCSolution parseInput part1
-  printTestSolutions 6 $ MkAoCSolution parseInput part2
+  printSolutions 6 $ MkAoCSolution parseInput part2
 
 parseInput :: Parser (Grid Char)
 parseInput = do
@@ -50,9 +51,13 @@ part1 input = numberOfDistinctGuardPositions $ runMachine state
     state = initState input
 
 --Dang this goes forever at the moment
-part2 input = testCycleDetection state
+part2 input = length $ filter (hasCycle . addObstacleToState state) newObstacles 
   where
     state = initState input
+    newObstacles = enumeratePossibleNewObstacles state
+    testObstacles = [V2 3 6, V2 6 7, V2 7 7, V2 1 8, V2 3 8, V2 7 9]
+    --[True,False,True,False,False,False] (so only two of these are cycle-inducing according to my code!)
+    startingStates = map (addObstacleToState state) $ enumeratePossibleNewObstacles state
 
 initState :: Grid Char -> State
 initState grid = MkState guard North obstacles (V2 xMax yMax)
@@ -102,8 +107,36 @@ directionToUnitVector South = unit _y
 directionToUnitVector East = unit _x
 directionToUnitVector West = -(unit _x)
 
+stepStateAndCheckInBounds :: State -> Maybe State
+stepStateAndCheckInBounds state = let newState = stepState state
+                                  --in traceLns (renderState newState) $ if isInBounds newState then Just newState else Nothing
+                                  in if isInBounds newState then Just newState else Nothing
+
 --Test adding an obstacle at (4,9) which should cause a cycle
-testCycleDetection :: State -> Maybe (CycleData State)
-testCycleDetection s = hareAndTortoiseWithTermination stepState withObstacle id (not . isInBounds)
-  where withObstacle = addObstacle s (V2 4 9)
-        h = hareAndTortoiseWithTermination stepState withObstacle id (not . isInBounds)
+hasCycle :: State -> Bool
+hasCycle s = hareAndTortoiseM stepStateAndCheckInBounds s id
+
+renderState :: State -> String
+renderState MkState{..} = rendered
+  where guardChar = case _direction of
+                      North -> '^'
+                      South -> 'v'
+                      East -> '>'
+                      West -> '<'
+        gridMap = M.fromList [(_guard, guardChar)]
+        allObstacles = M.fromSet (const '#') _obstacles
+        allPoints = M.union gridMap allObstacles
+        rendered = renderVectorMap allPoints
+
+enumeratePossibleNewObstacles :: State -> [Point]
+enumeratePossibleNewObstacles s = S.toList newObstacles
+  where allPoints = S.fromList $ [V2 x y | x <- [0..s._grid^._x], y <- [0..s._grid^._y]]
+        newObstacles = S.difference allPoints (S.insert s._guard s._obstacles)
+
+addObstacleToState :: State -> Point -> State
+addObstacleToState s@MkState{..} loc = s { _obstacles = S.insert loc _obstacles }
+
+
+-- Coords of cycle-inducing obstacles according to puzzle
+-- [V2 3 6, V2 6 7, V2 7 7, V2 1 8, V2 3 8, V2 7 9]
+-- The ones I think are cycle inducing but aren't: (4 4), (5 1), (7 1)
