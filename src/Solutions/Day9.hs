1092309bf8dc04f0e2d9f2e77c1b7a6da557c08b
Raphael <rafi.colman@googlemail.com>
2024-12-10 13:42:05 +0000

    Day 9 part 2
    Had to refactor significantly to make it fast enough. This broke part 1
    which I might fix later.

--- src/Solutions/Day9.hs
+++ src/Solutions/Day9.hs
@@ -11,33 +11,34 @@ import Common.AoCSolutions
     printSolutions,
     printTestSolutions,
   )
-import Control.Lens (makeLenses)
+import Common.ListUtils (window2)
+import Control.Lens (makeLenses, Bifunctor (bimap))
+import Control.Monad.State (execState, modify, runState)
 import Data.Char (digitToInt)
-import Data.Foldable (Foldable (foldl'), find)
-import qualified Data.IntMap as IM
+import Data.Foldable (Foldable (foldl'), find, foldrM)
+import qualified Data.IntMap.Strict as IM
+import Data.Maybe (isNothing)
+import Debug.Trace
 import Text.Parser.Combinators (some)
 import Text.Trifecta (Parser, digit, integer)
-import Data.Maybe (isNothing)
-import Control.Monad.State (runState, modify)
-import GHC.RTS.Flags (ProfFlags(heapProfileInterval))
 
 data ReadState = MkReadState
   { _readFileBlock :: !Bool, -- If this is false we're reading free space
-    _flattenedDisk :: !(IM.IntMap Integer),
+    _flattenedDisk :: !Disk,
     _currentFileId :: !Integer,
     _currentIndex :: !Int
   }
   deriving (Eq, Show)
 
-type Disk = IM.IntMap Integer
+type Disk = IM.IntMap (Integer, Integer) -- (FileId, Length)
 
 makeLenses ''ReadState
 
 aoc9 :: IO ()
 aoc9 = do
-  printSolutions 9 $ MkAoCSolution parseInput part1
+  -- printSolutions 9 $ MkAoCSolution parseInput part1
+  printSolutions 9 $ MkAoCSolution parseInput part2
 
--- printSolutions 9 $ MkAoCSolution parseInput part2
 --
 --
 type DiskMap = [Integer]
@@ -47,15 +48,15 @@ parseInput = do
   digits <- some digit
   pure $ map (toInteger . digitToInt) digits
 
-part1 input = checkSum solved
+-- part1 input = checkSum solved
+--   where
+--     solved = compress $ readDiskMap input
+
+part2 input = checkSum solved
   where
-    test = [9, 0, 9, 0, 9]
-    solved = compress $ readDiskMap input
+    solved = compressPreservingFiles $ readDiskMap input
 
-part2 :: String -> String
-part2 = undefined
-
-readDiskMap :: DiskMap -> IM.IntMap Integer
+readDiskMap :: DiskMap -> Disk
 readDiskMap dm = _flattenedDisk $ foldl' go (MkReadState True IM.empty 0 0) dm
   where
     go MkReadState {..} i =
@@ -65,15 +66,8 @@ readDiskMap dm = _flattenedDisk $ foldl' go (MkReadState True IM.empty 0 0) dm
            in MkReadState False newMp (_currentFileId + 1) (_currentIndex + fromIntegral i)
         else MkReadState True _flattenedDisk _currentFileId (_currentIndex + fromIntegral i)
 
-insertFileId :: Integer -> Int -> Int -> IM.IntMap Integer -> IM.IntMap Integer
-insertFileId fileId startIdx times mp = foldl' (\m i -> IM.insert i fileId m) mp [startIdx .. startIdx + times - 1]
-
--- | For debug purposes
-renderDiskMap :: IM.IntMap Integer -> String
-renderDiskMap m = concatMap (\k -> IM.findWithDefault "." k m') [0 .. maxKey]
-  where
-    (maxKey, a) = IM.findMax m
-    m' = IM.map show m
+insertFileId :: Integer -> Int -> Integer -> Disk -> Disk
+insertFileId fileId startIdx times = IM.insert startIdx (fileId, times)
 
 isContiguous :: IM.IntMap a -> Bool
 isContiguous m = length keys == length [minKey .. maxKey]
@@ -86,21 +80,67 @@ isContiguous m = length keys == length [minKey .. maxKey]
 -- It would be faster to keep track of where the last free space was and search from there
 -- instead
 firstFreeSpace :: IM.IntMap a -> Maybe IM.Key
-firstFreeSpace m = find (\k -> isNothing (IM.lookup k m)) [0..maxKey]
+firstFreeSpace m = find (\k -> isNothing (IM.lookup k m)) [0 .. maxKey]
   where
     (maxKey, _) = IM.findMax m
+
+firstFreeSpace' :: Int -> Int -> Disk -> Maybe (Int, Int)
+firstFreeSpace' spaceRequired originalStartingIndex m = find (\(idx, size) -> size >= spaceRequired && idx < originalStartingIndex) gaps
+  where
+    gaps = calculateGaps m
 
 compressStep :: Disk -> Maybe Disk
 compressStep d = do
   firstFree <- firstFreeSpace d
   (maxKey, val) <- IM.lookupMax d
   let (_, newDisk) = flip runState d $ do
-                          modify $ IM.delete maxKey
-                          modify $ IM.insert firstFree val
+        modify $ IM.delete maxKey
+        modify $ IM.insert firstFree val
   pure newDisk
 
-compress :: Disk -> Disk
-compress d = maybe d compress (compressStep d)
+-- compress :: Disk -> Maybe Disk
+-- compress d = maybe d compress (compress d)
+
+-- I think foldrWithKey doesn't actually do it in reverse which is what I thought it would do
+compressPreservingFiles :: Disk -> Disk
+compressPreservingFiles disk = IM.foldrWithKey go disk disk
+  where
+    go :: Int -> (Integer, Integer) -> Disk -> Disk
+    go idx (fileId, size) d = case firstFreeSpace' (fromIntegral size) idx d of
+      Just (gapIdx, gapSize) -> flip execState d $ do
+        modify $ IM.delete idx
+        modify $ IM.insert gapIdx (fileId, size)
+      Nothing -> d
+--- nearly. This doesn't care if the gap is actually to the right of the original location
 
 checkSum :: Disk -> Integer
-checkSum = IM.foldrWithKey' (\k a acc -> acc + (toInteger k * a)) 0 
+checkSum = IM.foldrWithKey go 0
+  where
+    go :: Int -> (Integer, Integer) -> Integer -> Integer
+    go idx (fileId, size) acc = acc + sum (map (fileId *) indexes)
+      where indexes = map fromIntegral [idx .. (idx + fromIntegral size) - 1]
+
+-- This needs to be redefined
+
+-- | List of gaps of the form [(idx of gap, size of gap)]
+calculateGaps :: Disk -> [(Int, Int)]
+calculateGaps d = g
+  where
+    pairs = window2 $ IM.toList d
+    g =
+      map
+        ( \((idx, (_, size)), (nextIdx, _)) ->
+            let gapSize = (idx + fromIntegral size)
+             in (idx + (fromIntegral size), nextIdx - gapSize)
+        )
+        pairs
+
+---Gaps seem wrong for this
+--[(0,(0,2)),(2,(9,2)),(5,(1,3)),(11,(2,1)),(15,(3,3)),(19,(4,2)),(22,(5,4)),(27,(6,4)),(32,(7,3)),(36,(8,4))]
+--calculated: [(2,0),(6,1),(13,3),(23,3),(33,1),(40,1),(48,1),(58,1),(67,1)]
+--this is definitely wrong. There should be a gap of (4,1)
+-- >>> testGaps
+-- [(2,0),(4,1),(8,3),(12,3),(18,1),(21,1),(26,1),(31,1),(35,1)]
+testGaps :: [(Int, Int)]
+testGaps = calculateGaps test
+  where test = IM.fromList [(0, (0, 2)), (2, (9, 2)), (5, (1, 3)), (11, (2, 1)), (15, (3, 3)), (19, (4, 2)), (22, (5, 4)), (27, (6, 4)), (32, (7, 3)), (36, (8, 4))]
