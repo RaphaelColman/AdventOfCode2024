55c70fc2a4d14e9b09a7f45a18aed4794547441d
Raphael <rafi.colman@googlemail.com>
2024-12-11 21:55:09 +0000

    Day 10 cleanup

--- src/Solutions/Day10.hs
+++ src/Solutions/Day10.hs
@@ -16,16 +16,15 @@ import qualified Data.HashSet as HS
 import qualified Data.Map as M
 import Data.Maybe (mapMaybe)
 import qualified Data.Set as S
+import Debug.Trace
 import Linear.V2 (V2 (..))
 import Text.Trifecta (CharParsing (anyChar), Parser, some)
-import Debug.Trace
 
 type TrailMap = Grid Int
 
 aoc10 :: IO ()
 aoc10 = do
-  -- printSolutions 10 $ MkAoCSolution parseInput part1
-
+  printSolutions 10 $ MkAoCSolution parseInput part1
   printSolutions 10 $ MkAoCSolution parseInput part2
 
 parseInput :: Parser (Grid Int)
@@ -33,10 +32,11 @@ parseInput = do
   charGrid <- enumerateMultilineStringToVectorMap <$> some anyChar
   pure $ M.map digitToInt charGrid
 
--- part1 input = sum $ trailHeadScores input
+part1 input = sum $ trailHeadScores input
 
 part2 input = sum $ map (trailHeadRating input) sts
-  where sts = starts input
+  where
+    sts = starts input
 
 validNeighbours :: Grid Int -> V2 Int -> S.Set (V2 Int)
 validNeighbours grid pos =
@@ -50,20 +50,16 @@ destinations tm = M.filter (== 9) tm & M.keys
 starts :: TrailMap -> [V2 Int]
 starts tm = M.filter (== 0) tm & M.keys
 
-shortestPath :: TrailMap -> V2 Int -> V2 Int -> Maybe [V2 Int]
-shortestPath tm start end =
-  aStar
-    (HS.fromList . S.toList . validNeighbours tm) -- graph representation
-    (\a b -> 1) -- cost
-    (manhattanDistance end) -- heuristic
-    (== end) -- goal
-    start
-
-manhattanDistance :: V2 Int -> V2 Int -> Int
-manhattanDistance (V2 x1 y1) (V2 x2 y2) = abs (x1 - x2) + abs (y1 - y2)
-
 trailheadScore :: TrailMap -> V2 Int -> Int
-trailheadScore tm start = length $ mapMaybe (shortestPath tm start) $ destinations tm
+trailheadScore tm node = length $ go S.empty node
+  where
+    go :: S.Set (V2 Int) -> V2 Int -> S.Set (V2 Int)
+    go visited node
+      | tm M.! node == 9 = S.singleton node
+      | null nextNodes = S.empty
+      | otherwise = S.unions $ map (go visited) nextNodes
+      where
+        nextNodes = S.toList $ validNeighbours tm node
 
 trailHeadScores :: TrailMap -> [Int]
 trailHeadScores tm = map (trailheadScore tm) $ starts tm
