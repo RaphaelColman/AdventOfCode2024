9e5fea44ed27b6e979fd9d4e56c1fc1842b0169b
Raphael <rafi.colman@googlemail.com>
2024-12-11 07:55:41 +0000

    Day 11 part 1

--- src/Solutions/Day11.hs
+++ src/Solutions/Day11.hs
@@ -1,21 +1,39 @@
 module Solutions.Day11
-  ( aoc11
-  ) where
+  ( aoc11,
+  )
+where
 
-import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions)
-import           Text.Trifecta       (Parser)
+import Common.AoCSolutions
+  ( AoCSolution (MkAoCSolution),
+    printSolutions,
+    printTestSolutions,
+  )
+import Data.Function ((&))
+import Text.Parser.Combinators (some)
+import Text.Trifecta (Parser, integer)
 
 aoc11 :: IO ()
 aoc11 = do
-  printSolutions 11 $ MkAoCSolution parseInput part1
+  --printSolutions 11 $ MkAoCSolution parseInput part1
   printSolutions 11 $ MkAoCSolution parseInput part2
 
-parseInput :: Parser String
-parseInput = undefined
+parseInput :: Parser [Integer]
+parseInput = some integer
 
-part1 :: String -> String
-part1 = undefined
+part1 input = length $ (iterate blink input) !! 25
 
-part2 :: String -> String
-part2 = undefined
+part2 input = length $ (iterate blink input) !! 75
+--lol that's not coming back any time soon!
+
+stepStone :: Integer -> [Integer]
+stepStone i
+  | i == 0 = [1]
+  | even (length asDigitString) =
+      let (a, b) = splitAt (length asDigitString `div` 2) asDigitString
+       in [read a, read b]
+  | otherwise = [i * 2024]
+  where
+    asDigitString = show i
+
+blink :: [Integer] -> [Integer]
+blink = concatMap stepStone
