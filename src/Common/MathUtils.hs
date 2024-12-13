63617d1232dbb00f08fc45f22d0a91a172f9f70b
Raphael <rafi.colman@googlemail.com>
2024-12-13 21:19:52 +0000

    Day 13 parts 1 and 2

--- src/Common/MathUtils.hs
+++ src/Common/MathUtils.hs
@@ -1,4 +1,11 @@
 module Common.MathUtils where
+import Data.Ratio (denominator, numerator)
 
 triangleX :: (Integral a) => a -> a
 triangleX x = x * (x + 1) `div` 2
+
+isInteger :: Rational -> Bool
+isInteger x = denominator x == 1
+
+rationalToInteger :: Rational -> Maybe Integer
+rationalToInteger x = if isInteger x then Just $ numerator x else Nothing
--- src/Solutions/Day13.hs
+++ src/Solutions/Day13.hs
@@ -1,21 +1,80 @@
-module Solutions.Day13
-  ( aoc13
-  ) where
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
 
-import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions)
-import           Text.Trifecta       (Parser)
+module Solutions.Day13
+  ( aoc13,
+  )
+where
+
+import Common.AoCSolutions
+  ( AoCSolution (MkAoCSolution),
+    printSolutions,
+    printTestSolutions,
+  )
+import Control.Applicative (Alternative ((<|>)))
+import Control.Lens (makeLenses)
+import Linear (V2 (V2))
+import Text.Trifecta (CharParsing (char, string), Parser, count, integer, some)
+import Common.MathUtils (isInteger, rationalToInteger)
+import Control.Monad (guard)
+import Data.Maybe (catMaybes, mapMaybe)
+import Control.Lens.Combinators (over)
+
+data Machine = MkMachine
+  { _buttonA :: !(V2 Integer), -- Costs 3 tokens
+    _buttonB :: !(V2 Integer), -- Costs 1 token
+    _prizeLocation :: !(V2 Integer)
+  }
+  deriving (Eq, Show)
+
+makeLenses ''Machine
 
 aoc13 :: IO ()
 aoc13 = do
   printSolutions 13 $ MkAoCSolution parseInput part1
   printSolutions 13 $ MkAoCSolution parseInput part2
 
-parseInput :: Parser String
-parseInput = undefined
+--
+parseInput :: Parser [Machine]
+parseInput = some parseMachine
 
-part1 :: String -> String
-part1 = undefined
+parseMachine :: Parser Machine
+parseMachine = do
+  [a, b] <- count 2 parseButton
+  MkMachine a b <$> parsePrize
 
-part2 :: String -> String
-part2 = undefined
+parseButton :: Parser (V2 Integer)
+parseButton = do
+  string "Button " >> (char 'A' <|> char 'B') >> string ": "
+  x <- string "X+" *> integer <* string ", "
+  y <- string "Y+" *> integer
+  pure $ V2 x y
+
+parsePrize :: Parser (V2 Integer)
+parsePrize = do
+  string "Prize: "
+  x <- string "X=" *> integer <* string ", "
+  y <- string "Y=" *> integer
+  pure $ V2 x y
+
+part1 input = sum $ map (uncurry tokenCost) solved
+  where solved = mapMaybe resolveMachine input
+
+part2 input = sum $ map (uncurry tokenCost) solved
+  where solved = mapMaybe (resolveMachine . modifyMachine) input
+
+resolveMachine :: Machine -> Maybe (Integer, Integer)
+resolveMachine (MkMachine (V2 ax ay) (V2 bx by) (V2 tx ty)) = do
+  a <- rationalToInteger a'
+  b <- rationalToInteger b'
+  pure (a, b)
+  where
+    b' :: Rational = fromIntegral (tx * ay - ty * ax) / fromIntegral (bx * ay - by * ax)
+    a' = (fromIntegral ty - (b' * fromIntegral by)) / fromIntegral ay
+
+tokenCost :: Integer -> Integer -> Integer
+tokenCost a b = 3 * a + b
+
+modifyMachine :: Machine -> Machine
+modifyMachine (MkMachine a b (V2 x y)) = MkMachine a b (V2 (x+10000000000000) (y+10000000000000))
