c91e240619268fb0eb87038d2ae43e802de0d625
Raphael <rafi.colman@googlemail.com>
2024-12-20 15:04:53 +0000

    Day 19 part 1

--- src/Solutions/Day19.hs
+++ src/Solutions/Day19.hs
@@ -8,13 +8,16 @@ import Common.AoCSolutions
     printSolutions,
     printTestSolutions,
   )
+import Data.Function ((&))
+import Data.List (isPrefixOf)
+import Data.Maybe (mapMaybe)
 import qualified Data.Set as S
 import Text.Parser.Combinators (some)
 import Text.Trifecta (CharParsing (anyChar, string), Parser, TokenParsing (token), commaSep, letter, manyTill)
 
 aoc19 :: IO ()
 aoc19 = do
-  printTestSolutions 19 $ MkAoCSolution parseInput part1
+  printSolutions 19 $ MkAoCSolution parseInput part1
 
 -- printSolutions 19 $ MkAoCSolution parseInput part2
 
@@ -28,8 +31,17 @@ parseInput = do
 parseTowels :: Parser [String]
 parseTowels = commaSep $ some letter
 
-part1 = id
-
+part1 (towels, patterns) = length $ filter (possiblePattern towels) patterns
 
 placeTowel :: String -> String -> Maybe String
-placeTowel pattern towel = undefined
+placeTowel pattern towel
+  | towel `isPrefixOf` pattern = Just $ drop (length towel) pattern
+  | otherwise = Nothing
+
+possiblePattern :: [String] -> String -> Bool
+possiblePattern towels = go
+  where
+    go :: String -> Bool
+    go pattern'
+      | null pattern' = True
+      | otherwise = mapMaybe (placeTowel pattern') towels & any go
