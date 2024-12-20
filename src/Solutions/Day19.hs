758d885b7fe6368281dd1200fa49525910704b48
Raphael <rafi.colman@googlemail.com>
2024-12-20 11:54:15 +0000

    Day 20 part 1

--- src/Solutions/Day19.hs
+++ src/Solutions/Day19.hs
@@ -1,26 +1,35 @@
 module Solutions.Day19
-  ( aoc19
-  ) where
+  ( aoc19,
+  )
+where
 
-import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions, printTestSolutions)
-import           Text.Trifecta       (Parser, commaSep, CharParsing (anyChar, string), manyTill, letter, TokenParsing (token))
+import Common.AoCSolutions
+  ( AoCSolution (MkAoCSolution),
+    printSolutions,
+    printTestSolutions,
+  )
 import qualified Data.Set as S
 import Text.Parser.Combinators (some)
+import Text.Trifecta (CharParsing (anyChar, string), Parser, TokenParsing (token), commaSep, letter, manyTill)
 
 aoc19 :: IO ()
 aoc19 = do
   printTestSolutions 19 $ MkAoCSolution parseInput part1
-  --printSolutions 19 $ MkAoCSolution parseInput part2
+
+-- printSolutions 19 $ MkAoCSolution parseInput part2
 
 parseInput :: Parser ([String], [String])
 parseInput = do
   towels <- parseTowels
   string "\n\n"
   stripeCombos <- some $ token $ some letter
-  pure $ (towels, stripeCombos)
+  pure (towels, stripeCombos)
 
 parseTowels :: Parser [String]
 parseTowels = commaSep $ some letter
 
 part1 = id
+
+
+placeTowel :: String -> String -> Maybe String
+placeTowel pattern towel = undefined
--- src/Solutions/Day20.hs
+++ src/Solutions/Day20.hs
@@ -1,21 +1,73 @@
-module Solutions.Day20
-  ( aoc20
-  ) where
+{-# LANGUAGE ScopedTypeVariables #-}
 
-import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions)
-import           Text.Trifecta       (Parser)
+module Solutions.Day20
+  ( aoc20,
+  )
+where
+
+import Common.AoCSolutions
+  ( AoCSolution (MkAoCSolution),
+    printSolutions,
+    printTestSolutions,
+  )
+import Common.Geometry (Grid, Point, allOrthogonalNeighbours, enumerateMultilineStringToVectorMap, gridOrthogonalNeighbours)
+import Common.ListUtils (windowN)
+import Control.Lens (makeLenses)
+import Control.Lens.Getter ((^.))
+import Data.Function ((&))
+import Data.List (unfoldr)
+import qualified Data.Map as M
+import qualified Data.Sequence as Seq
+import qualified Data.Set as S
+import Debug.Trace
+import Linear.V2 (V2 (V2), _x, _y)
+import Linear.Vector (unit)
+import Safe (headMay)
+import Text.Parser.Combinators (some)
+import Text.Printf (printf)
+import Text.Trifecta (CharParsing (anyChar), Parser)
 
 aoc20 :: IO ()
 aoc20 = do
   printSolutions 20 $ MkAoCSolution parseInput part1
-  printSolutions 20 $ MkAoCSolution parseInput part2
 
-parseInput :: Parser String
-parseInput = undefined
+-- printSolutions 20 $ MkAoCSolution parseInput part2
 
-part1 :: String -> String
-part1 = undefined
+parseInput :: Parser (Grid Char)
+parseInput = enumerateMultilineStringToVectorMap <$> some anyChar
 
-part2 :: String -> String
-part2 = undefined
+part1 input = sum cheats
+  where
+    cheatless = runRoute input
+    cheats = map (findCheat cheatless) [100 .. (length cheatless - 1)]
+
+-- | Run the route with no cheats
+runRoute :: Grid Char -> [V2 Int]
+runRoute grid = start : theRun
+  where
+    start = M.filter (== 'S') grid & M.keys & head
+    theRun = unfoldr go (start, start)
+    go :: (Point, Point) -> Maybe (Point, (Point, Point))
+    go (prev, pos) = do
+      let neighbours = gridOrthogonalNeighbours grid pos
+      nextStep <-
+        M.filterWithKey
+          (\k v -> (v `elem` ".E") && k /= prev)
+          neighbours
+          & M.keys
+          & headMay -- If we got nothing we hit the end
+      pure (nextStep, (pos, nextStep))
+
+twoSpacesApart :: Point -> Point -> Bool
+twoSpacesApart p1 p2 = p2 - p1 `elem` [V2 0 2, V2 0 (-2), V2 2 0, V2 (-2) 0]
+
+pairsNApart :: Int -> [a] -> [(a, a)]
+pairsNApart n xs = zip xs l2
+  where
+    l2 = drop n xs
+
+findCheat :: [V2 Int] -> Int -> Int
+findCheat route saved =
+  pairsNApart (saved + 2) route
+    & filter (uncurry twoSpacesApart)
+    & length
