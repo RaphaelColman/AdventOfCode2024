df2bec43926ad11e3d7f3f39a1694e9c8a7c50f3
Raphael <rafi.colman@googlemail.com>
2024-12-20 17:56:11 +0000

    Day 19 part 2

--- src/Solutions/Day19.hs
+++ src/Solutions/Day19.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Solutions.Day19
   ( aoc19,
   )
@@ -8,18 +10,22 @@ import Common.AoCSolutions
     printSolutions,
     printTestSolutions,
   )
-import Data.Function ((&))
-import Data.List (isPrefixOf)
+import qualified Common.Geometry
+import Control.Parallel.Strategies
+import Data.Function (fix, (&))
+import Data.List (inits, isPrefixOf, tails)
+import qualified Data.Map as M
 import Data.Maybe (mapMaybe)
 import qualified Data.Set as S
+import Debug.Trace
 import Text.Parser.Combinators (some)
+import Text.Printf (printf)
 import Text.Trifecta (CharParsing (anyChar, string), Parser, TokenParsing (token), commaSep, letter, manyTill)
 
 aoc19 :: IO ()
 aoc19 = do
   printSolutions 19 $ MkAoCSolution parseInput part1
-
--- printSolutions 19 $ MkAoCSolution parseInput part2
+  printSolutions 19 $ MkAoCSolution parseInput part2
 
 parseInput :: Parser ([String], [String])
 parseInput = do
@@ -31,17 +37,26 @@ parseInput = do
 parseTowels :: Parser [String]
 parseTowels = commaSep $ some letter
 
-part1 (towels, patterns) = length $ filter (possiblePattern towels) patterns
+part1 (towels, patterns) = length $ filter (> 0) counts
+  where
+    counts = map (countPatterns towels) patterns
+
+part2 (towels, patterns) = sum counts
+  where
+    counts = map (countPatterns towels) patterns
 
 placeTowel :: String -> String -> Maybe String
 placeTowel pattern towel
   | towel `isPrefixOf` pattern = Just $ drop (length towel) pattern
   | otherwise = Nothing
 
-possiblePattern :: [String] -> String -> Bool
-possiblePattern towels = go
+countPatterns :: [String] -> String -> Integer
+countPatterns towels pattern = memo M.! pattern
   where
-    go :: String -> Bool
-    go pattern'
-      | null pattern' = True
-      | otherwise = mapMaybe (placeTowel pattern') towels & any go
+    memo = M.fromList instr
+    instr = map go $ tails pattern
+    go [] = ("", 1)
+    go k = (k, total)
+      where
+        remaining = mapMaybe (placeTowel k) towels
+        total = sum $ map (memo M.!) remaining
