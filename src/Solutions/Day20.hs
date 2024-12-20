56c2e0ceb7dda24e71a4f8fc2ad1d635650bb94d
Raphael <rafi.colman@googlemail.com>
2024-12-20 14:27:25 +0000

    Day 20 part 2 (a bit messy)

--- src/Solutions/Day20.hs
+++ src/Solutions/Day20.hs
@@ -10,8 +10,8 @@ import Common.AoCSolutions
     printSolutions,
     printTestSolutions,
   )
-import Common.Geometry (Grid, Point, allOrthogonalNeighbours, enumerateMultilineStringToVectorMap, gridOrthogonalNeighbours)
-import Common.ListUtils (windowN)
+import Common.Geometry (Grid, Point, allOrthogonalNeighbours, enumerateMultilineStringToVectorMap, gridOrthogonalNeighbours, manhattanDistance)
+import Common.ListUtils (windowN, freqs)
 import Control.Lens (makeLenses)
 import Control.Lens.Getter ((^.))
 import Data.Function ((&))
@@ -26,20 +26,34 @@ import Safe (headMay)
 import Text.Parser.Combinators (some)
 import Text.Printf (printf)
 import Text.Trifecta (CharParsing (anyChar), Parser)
+import Data.Maybe (mapMaybe)
 
 aoc20 :: IO ()
 aoc20 = do
-  printSolutions 20 $ MkAoCSolution parseInput part1
-
--- printSolutions 20 $ MkAoCSolution parseInput part2
+  -- printSolutions 20 $ MkAoCSolution parseInput part1
+  printSolutions 20 $ MkAoCSolution parseInput part2
 
 parseInput :: Parser (Grid Char)
 parseInput = enumerateMultilineStringToVectorMap <$> some anyChar
 
-part1 input = sum cheats
+part1 = solve 2
+
+part2 input = length $ filter (>= 100) foo
   where
     cheatless = runRoute input
-    cheats = map (findCheat cheatless) [100 .. (length cheatless - 1)]
+    cheats = map (numberOfCheats cheatless 20) examples
+    examples = [50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76]
+    foo = concatMap (savings cheatless) [2..(length cheatless - 1)]
+
+solve cheatDistance input = sum cheats
+  where
+    cheatless = runRoute input
+    cheats = map (numberOfCheats cheatless cheatDistance) [76 .. (length cheatless - 1)]
+
+solve' input = length $ filter (>= 100) foo
+  where
+    cheatless = runRoute input
+    foo = concatMap (savings cheatless) [2..(length cheatless - 1)]
 
 -- | Run the route with no cheats
 runRoute :: Grid Char -> [V2 Int]
@@ -61,13 +75,24 @@ runRoute grid = start : theRun
 twoSpacesApart :: Point -> Point -> Bool
 twoSpacesApart p1 p2 = p2 - p1 `elem` [V2 0 2, V2 0 (-2), V2 2 0, V2 (-2) 0]
 
+manhattanDistanceWithMax :: Int -> Point -> Point -> Maybe Int
+manhattanDistanceWithMax max p1 p2 = let d = manhattanDistance p1 p2 in if d > max then Nothing else Just d
+
 pairsNApart :: Int -> [a] -> [(a, a)]
 pairsNApart n xs = zip xs l2
   where
     l2 = drop n xs
 
-findCheat :: [V2 Int] -> Int -> Int
-findCheat route saved =
+numberOfCheats :: [V2 Int] -> Int -> Int -> Int
+numberOfCheats route cheatDistance saved =
   pairsNApart (saved + 2) route
     & filter (uncurry twoSpacesApart)
     & length
+
+-- | Returns a list of the possible ps saved for this route and skipLength
+savings :: [V2 Int] -> Int -> [Int]
+savings route skipLength = map (skipLength -) mDistances
+  where
+    mDistances =
+      pairsNApart skipLength route
+        & mapMaybe (uncurry (manhattanDistanceWithMax 20))
