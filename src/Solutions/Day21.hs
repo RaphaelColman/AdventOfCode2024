c68fa32b3058d9e3b9da1cfc26eb032e08935f85
Raphael <rafi.colman@googlemail.com>
2024-12-23 18:30:02 +0000

    Day 21 part 1

--- src/Solutions/Day21.hs
+++ src/Solutions/Day21.hs
@@ -5,91 +5,89 @@ module Solutions.Day21
   )
 where
 
+import Combinatorics (permute, permuteRep, variate, variateRep)
 import Common.AoCSolutions
   ( AoCSolution (MkAoCSolution),
     printSolutions,
     printTestSolutions,
   )
+import Common.FunctorUtils (fmap2)
 import Common.Geometry (Point)
 import Common.ListUtils (window2)
 import Data.Char (isDigit)
+import Data.Function ((&))
+import Data.List (minimumBy, scanl')
 import qualified Data.Map as M
 import Data.MemoTrie (memo2)
-import Debug.Trace (traceShow)
+import Data.Ord (comparing)
+import Debug.Trace (trace, traceShow)
 import Linear (unit, _x, _y)
 import Linear.V2 (V2 (V2))
 import Text.Parser.Combinators (some)
 import Text.Parser.Token (token)
+import Text.Printf (printf)
 import Text.Trifecta (CharParsing (anyChar), Parser, alphaNum, letter)
-import Combinatorics (permute)
 
 type Keymap = M.Map Char (V2 Int)
 
 aoc21 :: IO ()
-aoc21 = do
-  printTestSolutions 21 $ MkAoCSolution parseInput part1
+aoc21 = printTestSolutions 21 $ MkAoCSolution parseInput part1
 
 -- printSolutions 21 $ MkAoCSolution parseInput part2
 
 parseInput :: Parser [String]
 parseInput = some $ token $ some alphaNum
 
-part1 input = numpadKeyPaths '3' '7'
+part1 input = sum $ map complexity input
 
-numpadKeyPath :: Char -> Char -> [Char]
-numpadKeyPath start end = rights ++ ups ++ lefts ++ downs ++ "A"
+bestInput :: Integer -> String -> String
+bestInput level input
+  | level == 0 = input
+  | otherwise = concatMap (minimumBy (comparing length)) asBestInput
   where
-    (V2 x y) = numpadKeys M.! end - numpadKeys M.! start
-    rights = replicate x '>'
-    downs = replicate y 'v'
-    lefts = replicate (-x) '<'
-    ups = replicate (-y) '^'
-    numpadKeys = M.fromList $ zip "789456123 0A" pts
-      where
-        pts = [V2 x y | y <- [0 .. 3], x <- [0 .. 2]]
+    pairs = window2 ('A' : input)
+    m = map (\(a, b) -> allPaths M.! (a, b)) pairs
+    asBestInput = fmap2 (bestInput (level - 1)) m
+
+complexity :: String -> Integer
+complexity code = toInteger (length dirpath) * numPart
+  where
+    numPart :: Integer = read $ takeWhile isDigit code
+    dirpath = bestInput 3 code
+
+unitVectorToChar :: V2 Int -> Char
+unitVectorToChar v = case v of
+  V2 0 1 -> 'v'
+  V2 0 (-1) -> '^'
+  V2 1 0 -> '>'
+  V2 (-1) 0 -> '<'
 
 numpadKeyPaths :: Char -> Char -> [String]
-numpadKeyPaths start end = permute asChars --dang. <<vv appears twice (technically two different combos)
---Maybe we can do this with some sort of cartesian
+numpadKeyPaths start end = (++ "A") <$> fmap2 unitVectorToChar validPaths
   where
     (V2 x y) = numpadKeys M.! end - numpadKeys M.! start
-    xMoves = replicate (abs x) (V2 (signum x) 0)
-    yMoves = replicate (abs y) (V2 0 (signum y))
-    asChars = map unitVectorToChar $ xMoves ++ yMoves
+    paths = permuteRep [(V2 (signum x) 0, abs x), (V2 0 (signum y), abs y)]
+    validPaths = filter (\path -> V2 0 3 `notElem` nodesVisited path) paths
+    nodesVisited = scanl' (+) (numpadKeys M.! start)
     numpadKeys = M.fromList $ zip "789456123 0A" pts
       where
         pts = [V2 x y | y <- [0 .. 3], x <- [0 .. 2]]
 
-dirpadKeyPath :: Char -> Char -> [Char]
-dirpadKeyPath start end = downs ++ rights ++ ups ++ lefts ++ "A"
+dirpadKeyPaths :: Char -> Char -> [String]
+dirpadKeyPaths start end = (++ "A") <$> fmap2 unitVectorToChar validPaths
   where
     (V2 x y) = dirpadKeys M.! end - dirpadKeys M.! start
-    rights = replicate x '>'
-    downs = replicate y 'v'
-    lefts = replicate (-x) '<'
-    ups = replicate (-y) '^'
+    paths = permuteRep [(V2 (signum x) 0, abs x), (V2 0 (signum y), abs y)]
+    validPaths = filter (\path -> V2 0 0 `notElem` nodesVisited path) paths
+    nodesVisited = scanl' (+) (dirpadKeys M.! start)
     dirpadKeys = M.fromList $ zip " ^A<v>" pts
       where
         pts = [V2 x y | y <- [0 .. 1], x <- [0 .. 2]]
 
-expandCode :: String -> String
-expandCode code = concatMap (uncurry numpadKeyPath) $ window2 ('A' : code)
-
-expandDirpath :: String -> String
-expandDirpath dirpath = concatMap (uncurry dirpadKeyPath) $ window2 ('A' : dirpath)
-
-fullSequence :: String -> String
-fullSequence = expandDirpath . expandDirpath . expandCode
-
-complexity :: String -> (Int, Int)
-complexity code = (length dirpath, numPart)
+allPaths :: M.Map (Char, Char) [String]
+allPaths = M.union numMap dirMap
   where
-    numPart :: Int = read $ takeWhile isDigit code
-    dirpath = fullSequence code
-
-unitVectorToChar :: V2 Int -> Char
-unitVectorToChar v = case v of
-  V2 0 1 -> '^'
-  V2 0 (-1) -> 'v'
-  V2 1 0 -> '>'
-  V2 (-1) 0 -> '<'
+    numKeys = variateRep 2 "1234567890A"
+    numMap = M.fromList $ map (\[a, b] -> ((a, b), numpadKeyPaths a b)) numKeys
+    dirKeys = variateRep 2 " ^A<v>^"
+    dirMap = M.fromList $ map (\[a, b] -> ((a, b), dirpadKeyPaths a b)) dirKeys
