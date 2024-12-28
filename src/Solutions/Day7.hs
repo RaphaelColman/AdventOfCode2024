7094aa49b1086290ea3f7eb5f959d866dafa2a37
Raphael <rafi.colman@googlemail.com>
2024-12-28 18:16:51 +0000

    Refactor day 7 to make it neater and way faster

--- src/Solutions/Day7.hs
+++ src/Solutions/Day7.hs
@@ -9,18 +9,21 @@ import Common.AoCSolutions
     printSolutions,
     printTestSolutions,
   )
+import Control.Monad (guard)
+import Data.List (isSuffixOf)
+import Data.Maybe (catMaybes, mapMaybe)
 import Text.Parser.Char (CharParsing (char, string))
 import Text.Parser.Token (integer, integer')
 import Text.Trifecta (Parser, Parsing (eof), TokenParsing (token), manyTill, newline, sepBy, some, try)
-import Data.Maybe (catMaybes, mapMaybe)
-import Control.Parallel.Strategies
 
 aoc7 :: IO ()
 aoc7 = do
-  printTestSolutions 7 $ MkAoCSolution parseInput part1
-  --printSolutions 7 $ MkAoCSolution parseInput part2
+  printSolutions 7 $ MkAoCSolution parseInput part1
+  printSolutions 7 $ MkAoCSolution parseInput part2
 
 type Equation = (Integer, [Integer])
+
+type OpTest = Integer -> Integer -> Maybe Integer
 
 parseInput :: Parser [Equation]
 parseInput = some $ token parseEquation
@@ -31,57 +34,36 @@ parseEquation = do
   inputs <- sepBy integer' $ char ' '
   pure (x, inputs)
 
--- At the moment this just does part 2.
-part1 input = sum $ map fst validEquations
+part1 :: [Equation] -> Integer
+part1 = solve [testMultiply, testAdd]
+
+part2 :: [Equation] -> Integer
+part2 = solve [testMultiply, testAdd, testConcat]
+
+solve :: [OpTest] -> [Equation] -> Integer
+solve tests input = sum $ map fst validEquations
   where
-    validEquations = filter equationIsValid input
+    validEquations = filter (equationIsValid tests) input
 
-part2 input = sum $ map fst validEquations
+equationIsValid :: [OpTest] -> Equation -> Bool
+equationIsValid tests (target, first : rest) = elem first $ foldr go [target] rest
   where
-    validEquations = filter equationIsValid input `using` parList rdeepseq
+    go :: Integer -> [Integer] -> [Integer]
+    go a acc = do
+      current <- acc
+      mapMaybe (\f -> f current a) tests
 
-data Expression
-  = Const Integer
-  | Add
-  | Mul
-  | Concat
+testMultiply :: OpTest
+testMultiply x y = let (quotient, remainder) = x `quotRem` y in if remainder == 0 then Just quotient else Nothing
 
-instance Show Expression where
-  show (Const x) = show x
-  show Add = "+"
-  show Mul = "*"
-  show Concat = "||"
+testAdd :: OpTest
+testAdd x y = let result = x - y in if result >= 0 then Just result else Nothing
 
--- >>> allOrders 2
--- [[+,+],[+,*],[*,+],[*,*]]
-allOrders :: Int -> [[Expression]]
-allOrders length = variateRep length [Add, Concat, Mul]
-
-generateExpressionLists :: [Integer] -> [[Expression]]
-generateExpressionLists xs = map equation $ allOrders (length xs - 1)
+testConcat :: OpTest
+testConcat x y = do
+  guard $ x /= y
+  if yString `isSuffixOf` xString then Just (read (take numCharsToKeep xString)) else Nothing
   where
-    equation = alternate (map Const xs)
-
-evaluate :: Integer -> [Expression] -> Maybe Integer
-evaluate target [Const i] = Just i
-evaluate target (Const x : Add : Const y : rest) = if x > target then Nothing else evaluate target $ Const (x + y) : rest
-evaluate target (Const x : Mul : Const y : rest) = if x > target then Nothing else evaluate target $ Const (x * y) : rest
-evaluate target (Const x : Concat : Const y : rest) = if x > target then Nothing else evaluate target $ Const (concatenateIntegers x y) : rest
---There has GOT to be a neater way of doing this!
-
--- | Can the test values be evaluated to the target value?
-equationIsValid :: Equation -> Bool
-equationIsValid (target, ints) = target `elem` results
-  where
-    results = mapMaybe (evaluate target) $ generateExpressionLists ints
-
--- >>> alternate [1..3] [7..10]
--- [1,7,2,8,3,9,10]
-alternate :: [a] -> [a] -> [a]
-alternate [] [] = []
-alternate xs [] = xs
-alternate [] ys = ys
-alternate (x : xs) (y : ys) = x : y : alternate xs ys
-
-concatenateIntegers :: Integer -> Integer -> Integer
-concatenateIntegers x y = read $ show x ++ show y
+    xString = show x
+    yString = show y
+    numCharsToKeep = length xString - length yString
